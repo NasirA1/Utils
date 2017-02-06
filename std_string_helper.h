1a6b977132bc51f5e502e667ec555032b1cd3e37
NasirA1 <pusarlai@msn.com>
2017-02-06 21:05:06 +0000

    enhanced trim functions

--- std_string_helper.h
+++ std_string_helper.h
@@ -6,6 +6,10 @@
 #include <iostream>
 #include <locale>
 #include <windows.h>
+#include <array>
+#include <vector>
+#include <sstream>
+
 
 
 //Returns C-style formatted std string
@@ -69,6 +73,7 @@ inline static basic_string<CHARTYPE>& trim(basic_string<CHARTYPE>& s, const std:
 	return trimr(triml(s, trim_chars), trim_chars);
 }
 
+
 template<class CHARTYPE = char>
 inline static bool starts_with(const basic_string<CHARTYPE>& subject, const basic_string<CHARTYPE>& prefix)
 {
@@ -84,6 +89,63 @@ inline static bool starts_with(const CHARTYPE* const subject, const CHARTYPE* co
 	return starts_with(s_subject, s_prefix);
 }
 
+template<class CHARTYPE = char>
+inline static bool starts_with(const basic_string<CHARTYPE>& subject, const CHARTYPE* const prefix)
+{
+	const basic_string<CHARTYPE> s_prefix(prefix);
+	return starts_with(subject, s_prefix);
+}
+
+
+template<class CHARTYPE = char>
+inline static bool ends_with(const basic_string<CHARTYPE>& subject, const basic_string<CHARTYPE>& suffix)
+{
+	if (suffix.length() > subject.length()) return false;
+	return subject.substr(subject.length() - suffix.length(), suffix.length()) == suffix;
+}
+
+template<class CHARTYPE = char>
+inline static bool ends_with(const CHARTYPE* const subject, const CHARTYPE* const suffix)
+{
+	const basic_string<CHARTYPE> s_subject(subject);
+	const basic_string<CHARTYPE> s_suffix(suffix);
+	return ends_with(s_subject, s_suffix);
+}
+
+template<class CHARTYPE = char>
+inline static bool ends_with(const basic_string<CHARTYPE>& subject, const CHARTYPE* const suffix)
+{
+	const basic_string<CHARTYPE> s_suffix(suffix);
+	return ends_with(subject, suffix);
+}
+
+
+template<class CHARTYPE = char>
+inline static basic_string<CHARTYPE> copy(const basic_string<CHARTYPE>& s)
+{
+	return s;
+}
+
+template<class CHARTYPE = char>
+inline static basic_string<CHARTYPE> copy(const CHARTYPE* const s)
+{
+	return s;
+}
+
+template<class CHARTYPE = char, size_t ARRSIZE = 1>
+inline static auto split(const basic_string<CHARTYPE>& subject, const CHARTYPE delim = '\n', const bool ignore_empty = false)
+{
+	std::vector<std::basic_string<CHARTYPE>> output;
+	std::basic_istringstream<CHARTYPE> iss(subject);
+
+	for (std::basic_string<CHARTYPE> token; std::getline(iss, token, delim);)
+	{
+		if(!token.empty())
+			output.push_back(std::move(token));
+	}
+
+	return output;
+}
 
 
 #pragma region format() Helpers

072359036ba30540a3cc6777caa188b8bfdecb20
NasirA1 <pusarlai@msn.com>
2017-02-12 00:58:51 +0000

    refactor

--- std_quick_files.h
+++ std_quick_files.h
@@ -5,59 +5,70 @@
 
 ///Generic way of reading text files (works for both ASCII and UTF8 strings)
 
+
 using namespace std;
 
-namespace util
+
+//Return filename (without extension) from the given path
+template<class CHARTYPE = char>
+std::basic_string<CHARTYPE> filename_from_path(const std::basic_string<CHARTYPE>& fullpath)
 {
-  
-	//Reads all text from the given text file at once and returns result in a string
-  template<typename TChar = char>
-  auto readAllText(const basic_string<TChar>& filename)
-  {
-    basic_ifstream<TChar> file(filename);
-    basic_string<TChar> str;
-
-    file.seekg(0, ios::end);
-    str.reserve(static_cast<size_t>(file.tellg()));
-
-    file.seekg(0, ios::beg);
-    str.assign((istreambuf_iterator<TChar>(file)), istreambuf_iterator<TChar>());
-    
-    return str;
-  }
-
-
-	//Reads all text from the given text file line by line and returns result in a vectror of strings
-  template<typename TChar = char>
-  auto readAllLines(const basic_string<TChar>& filename)
-  {
-    vector<basic_string<TChar>> lines;
-    basic_ifstream<TChar> file(filename);
-    
-    while (!file.eof())
-    {
-      basic_string<TChar> line;
-      getline(file, line);
-      lines.emplace_back(line);
-    }
-
-    return lines;
-  }
-
-
-	//Reads all text from the given text file at once and returns result in a string
-  template<typename TChar = char>
-  auto readAllText(const TChar* const filename)
-  {
-    return readAllText(basic_string<TChar>(filename));
-  }
-
-
-	//Reads all text from the given text file line by line and returns result in a vectror of strings
-  template<typename TChar = char>
-  auto readAllLines(const TChar* const filename)
-  {
-    return readAllLines(basic_string<TChar>(filename));
-  }
-
+	auto name_pos = fullpath.find_last_of('\\') + 1;
+	auto ext_pos = fullpath.find_last_of('.');
+	auto just_the_name = fullpath.substr(name_pos, ext_pos - name_pos);
+	return just_the_name;
 }
+
+
+//Reads all text from the given text file at once and returns result in a string
+template<typename TChar = char>
+auto readAllText(const basic_string<TChar>& filename)
+{
+	basic_ifstream<TChar> file(filename);
+	basic_string<TChar> str;
+
+	file.seekg(0, std::ios::end);
+	auto length = file.tellg();
+	if (length <= 0) return str;
+
+	file.seekg(0, ios::beg);
+	str.assign((istreambuf_iterator<TChar>(file)), istreambuf_iterator<TChar>());
+
+	return str;
+}
+
+
+//Reads all text from the given text file line by line and returns result in a vectror of strings
+template<typename TChar = char>
+auto readAllLines(const basic_string<TChar>& filename)
+{
+	vector<basic_string<TChar>> lines;
+	basic_ifstream<TChar> file(filename);
+
+	while (!file.eof())
+	{
+		basic_string<TChar> line;
+		getline(file, line);
+		lines.emplace_back(line);
+	}
+
+	return lines;
+}
+
+
+//Reads all text from the given text file at once and returns result in a string
+template<typename TChar = char>
+auto readAllText(const TChar* const filename)
+{
+	return readAllText(basic_string<TChar>(filename));
+}
+
+
+//Reads all text from the given text file line by line and returns result in a vectror of strings
+template<typename TChar = char>
+auto readAllLines(const TChar* const filename)
+{
+	return readAllLines(basic_string<TChar>(filename));
+}
+
+
--- std_string_helper.h
+++ std_string_helper.h
@@ -9,7 +9,7 @@
 #include <array>
 #include <vector>
 #include <sstream>
-
+#include <codecvt>
 
 
 //Returns C-style formatted std string
@@ -17,10 +17,91 @@
 #define _FN(...)	format(##__VA_ARGS__).append(1, '\n')
 
 
-
 using namespace std;
 
 
+#pragma region format() Helpers
+//Helper function used in format()
+//Base Template for CHARTYPEs
+template<class CHARTYPE>
+static inline int my_vsnprintf(
+	_Out_writes_(_BufferCount)    CHARTYPE*       const _Buffer,
+	_In_                          size_t          const _BufferCount,
+	_In_z_ _Printf_format_string_ CHARTYPE const* const _Format,
+	va_list           _ArgList
+)
+{
+	std::cerr << "ERROR: Unknown CHARTYPE!! Provide template specialisation!" << std::endl;
+	return -1;
+}
+
+//Helper function used in format()
+//Template Specialisation for ASCII strings (CHARTYPE = char)
+template<>
+static inline int my_vsnprintf(
+	_Out_writes_(_BufferCount)    char*       const _Buffer,
+	_In_                          size_t      const _BufferCount,
+	_In_z_ _Printf_format_string_ char const* const _Format,
+	va_list           _ArgList
+)
+{
+	return vsnprintf(_Buffer, _BufferCount, _Format, _ArgList);
+}
+
+//Helper function used in format()
+//Template Specialisation for Unicode strings (CHARTYPE = TCHAR)
+template<>
+static inline int my_vsnprintf(
+	_Out_writes_(_BufferCount)    TCHAR*          const _Buffer,
+	_In_                          size_t          const _BufferCount,
+	_In_z_ _Printf_format_string_ TCHAR const*    const _Format,
+	va_list           _ArgList
+)
+{
+	return vswprintf(_Buffer, _BufferCount, _Format, _ArgList);
+}
+
+#pragma endregion
+
+
+//C-style string formatting on arbitrary-length input
+//Supply larger InitialBufferSize for larger strings for better performance
+template<size_t InitialBufferSize = 1024u, class CHARTYPE>
+static inline std::basic_string<CHARTYPE> format(const CHARTYPE* fmt, ...)
+{
+	va_list args;
+
+	std::basic_string<CHARTYPE> result;
+	int rc = -1;
+	size_t length = InitialBufferSize;
+
+	while (true)
+	{
+		va_start(args, fmt);
+		result.resize(length);
+		rc = my_vsnprintf(const_cast<CHARTYPE*>(result.data()), length, fmt, args);
+		va_end(args);
+
+		if (rc > -1 && static_cast<unsigned int>(rc) < length)
+		{//all ok
+			result.resize(rc);
+			return result;
+		}
+
+		if (rc > -1)
+			//needed size returned, allocate 1 byte for the null terminator
+			length = rc + 1;
+		else
+			//Error occurred. Try doubling buffer size
+			length *= 2;
+	}
+
+	return result;
+}
+
+
+
+#if 0
 //Trim the given string from the left and return reference
 template<class CHARTYPE>
 inline static basic_string<CHARTYPE>& triml(basic_string<CHARTYPE>& s)
@@ -43,7 +124,7 @@ inline static basic_string<CHARTYPE>& trim(basic_string<CHARTYPE>& s)
 {
 	return trimr(triml(s));
 }
-
+#endif
 
 //Trim the given string from the left and return reference
 //By default SPACE characters are trimmed. Custom characters can be provided in trim_chars parameter.
@@ -149,81 +230,12 @@ inline static auto split(const basic_string<CHARTYPE>& subject, const CHARTYPE d
 }
 
 
-#pragma region format() Helpers
-//Helper function used in format()
-//Base Template for CHARTYPEs
-template<class CHARTYPE>
-int my_vsnprintf(
-	_Out_writes_(_BufferCount)    CHARTYPE*       const _Buffer,
-	_In_                          size_t          const _BufferCount,
-	_In_z_ _Printf_format_string_ CHARTYPE const* const _Format,
-	va_list           _ArgList
-)
+static inline std::string wstring_to_string(const std::wstring& wstr)
 {
-	std::cerr << "ERROR: Unknown CHARTYPE!! Provide template specialisation!" << std::endl;
-	return -1;
+	//use converter (.to_bytes: wstr->str, .from_bytes: str->wstr)
+	using convert_type = std::codecvt_utf8<wchar_t>;
+	std::wstring_convert<convert_type, wchar_t> converter;
+	std::string astr = converter.to_bytes(wstr);
+	return astr;
 }
 
-//Helper function used in format()
-//Template Specialisation for ASCII strings (CHARTYPE = char)
-template<>
-int my_vsnprintf(
-	_Out_writes_(_BufferCount)    char*       const _Buffer,
-	_In_                          size_t      const _BufferCount,
-	_In_z_ _Printf_format_string_ char const* const _Format,
-	va_list           _ArgList
-)
-{
-	return vsnprintf(_Buffer, _BufferCount, _Format, _ArgList);
-}
-
-//Helper function used in format()
-//Template Specialisation for Unicode strings (CHARTYPE = TCHAR)
-template<>
-int my_vsnprintf(
-	_Out_writes_(_BufferCount)    TCHAR*          const _Buffer,
-	_In_                          size_t          const _BufferCount,
-	_In_z_ _Printf_format_string_ TCHAR const*    const _Format,
-	va_list           _ArgList
-)
-{
-	return vswprintf(_Buffer, _BufferCount, _Format, _ArgList);
-}
-
-#pragma endregion
-
-
-//C-style string formatting on arbitrary-length input
-//Supply larger InitialBufferSize for larger strings for better performance
-template<size_t InitialBufferSize = 1024u, class CHARTYPE>
-std::basic_string<CHARTYPE> format(const CHARTYPE* fmt, ...)
-{
-	va_list args;
-
-	std::basic_string<CHARTYPE> result;
-	int rc = -1;
-	size_t length = InitialBufferSize;
-
-	while (true)
-	{
-		va_start(args, fmt);
-		result.resize(length);
-		rc = my_vsnprintf(const_cast<CHARTYPE*>(result.data()), length, fmt, args);
-		va_end(args);
-
-		if (rc > -1 && static_cast<unsigned int>(rc) < length)
-		{//all ok
-			result.resize(rc);
-			return result;
-		}
-
-		if (rc > -1)
-			//needed size returned, allocate 1 byte for the null terminator
-			length = rc + 1;
-		else
-			//Error occurred. Try doubling buffer size
-			length *= 2;
-	}
-
-	return result;
-}

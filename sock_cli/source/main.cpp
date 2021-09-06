67a5403da7dda1ce66c1f30b4df9a1c144b29b64
NasirA1 <pusarlai@msn.com>
2021-09-06 21:59:21 +0100

    got gttplib working

--- sock_cli/source/main.cpp
+++ sock_cli/source/main.cpp
@@ -7,27 +7,30 @@
 #include <netinet/in.h>
 #include <netdb.h> 
 #include <string>
-
-#define PORT 8080
-#define HOST "127.0.0.1"
-
+#include <iostream>
+#include "httplib.h"
 
 
-int client_test()
+#define PORT 80
+#define HOST "api.ipify.org"
+
+
+int socket_client_test()
 {
+    printf("Socket Client test started\n");
     int sockfd, n;
     struct sockaddr_in serv_addr;
     struct hostent* server;
 
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd < 0) {
-        perror("ERROR opening socket");
+        printf("ERROR opening socket\n");
 		return -1;
 	}
     
 	server = gethostbyname(HOST);
     if (server == NULL) {
-        fprintf(stderr,"ERROR, no such host\n");
+        printf("ERROR, no such host\n");
         return -1;
     }
     
@@ -37,32 +40,55 @@ int client_test()
     serv_addr.sin_port = htons(PORT);
     
 	if (connect(sockfd,(struct sockaddr*) &serv_addr,sizeof(serv_addr)) < 0) { 
-        perror("ERROR connecting");
+        printf("ERROR connecting\n");
 		return -1;
 	}
     
-	std::string request = "GET / HTTP/1.1\n\r\nHost: " HOST "\n\r\n";
+    std::string request = "GET / HTTP/1.1\r\nAccept: */*\r\nHost: " HOST "\r\nAccept-Encoding: gzip, deflate, br\r\nConnection: keep-alive\r\n\r\n";
+    printf("Sending request: %s\n", request.c_str());
     n = write(sockfd, request.c_str(), request.length());
     if (n < 0) {
-         perror("ERROR writing to socket");
+         printf("ERROR writing to socket\n");
 		 return -1;
 	}
     
     char buffer[1024 * 1024] = {0};
     n = read(sockfd, buffer, sizeof(buffer) - 1);
     if (n < 0) {
-         perror("ERROR reading from socket");
+         printf("ERROR reading from socket\n");
 		 return -1;
 	}
     
+    printf("Received response: %d bytes\n", n);
 	printf("%s\n", buffer);
     
 	close(sockfd);
 	return 0;
 }
 
+void httplib_test()
+{
+    std::cout << std::endl << "  ----- Started httplib test -----" << std::endl;
+    httplib::Client cli(HOST, PORT);
+    auto res = cli.Get("/");
+    if(res) {
+        std::cout << "Response Status: " << res->status << std::endl;
+        std::cout << "Response Body: " << res->body << std::endl;
+    }
+    else {
+        std::cout << "Error: " << res.error() << std::endl;
+    }
+    std::cout << std::endl << "  ----- Ended httplib test -----" << std::endl;
+}
+
 
 int main(int argc, char *argv[])
 {
-	return client_test();
+	printf(" *** App Started ****\n");
+
+	socket_client_test();
+    httplib_test();
+
+	printf(" *** App Ended ****\n");
+    return 0;
 }
